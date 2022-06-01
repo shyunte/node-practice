fcbbb6d31b8cc2c96ca26e83c55f34a642c69c18
shyun <96578042+shyunte@users.noreply.github.com>
2022-06-02 02:22:41 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,16 +7,22 @@ class User{
         this.body = body;
     }
     login(){
-        const body = this.body;
-        const {id, password} = UserStorage.getUserInfo(body.id);
+        const client = this.body;
+        const { id, password } = UserStorage.getUserInfo(client.id);
 
         if(id){  
-            if (id === body.id && password === body.password){
-                return {success: true};
+            if (id === client.id && password === client.password) {
+              return { success: true };
             }
             return {success: false, msg: "비밀번호가 틀렸습니다."};
         }
         return {success: false, msg: "없는 아이디 입니다."};
+    }
+
+    register(){
+        const client = this.body;
+        const response = UserStorage.save(client);
+        return response;
     }
 }
 module.exports = User;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,12 +19,13 @@ const process = {
     login : (req, res)=>{
         const user = new User(req.body);
         const response = user.login();
-        console.log(response);
         return res.json(response);
         
         
-        response.success = false;
-        response.msg = "로그인에 실패하였습니다."
+    },
+    register : (req, res)=>{
+        const user = new User(req.body);
+        const response = user.register();
         return res.json(response);
     },
 }; 
