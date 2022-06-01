335a384ff52bf1eacc66e0b06523ef2263fc18c6
shyun <96578042+shyunte@users.noreply.github.com>
2022-06-02 02:06:59 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    password = document.querySelector("#password"),
+    confirmPassword = document.querySelector("#confirm-password"),
+    registerBtn = document.querySelector("#button");
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+  const req = {
+    id: id.value,
+    name : name.value,
+    password: password.value,
+    confirmPassword: confirmPassword.value,
+  };
+  console.log(req);
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/login";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("회원가입 중 에러 발생"));
+    });
+}
+
+console.log(id);
