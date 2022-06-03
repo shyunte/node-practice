b29a1fa11234db4b51880df4f131ffe1308c0c28
shyun <96578042+shyunte@users.noreply.github.com>
2022-06-03 16:35:00 +0900

    fs통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -6,13 +6,14 @@ class User{
     constructor(body){
         this.body = body;
     }
-    login(){
+
+    async login(){
         const client = this.body;
-        const { id, password } = UserStorage.getUserInfo(client.id);
+        const { id , password } = await UserStorage.getUserInfo(client.id);
 
         if(id){  
             if (id === client.id && password === client.password) {
-              return { success: true };
+                return { success: true };
             }
             return {success: false, msg: "비밀번호가 틀렸습니다."};
         }
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,38 +1,44 @@
 "use strict";
 
-class UserStorage{
-    static #users = { // #은 public > private로 변경하는 것
-        id :["shyun", "sht123","sht2253"],
-        password :["123", "123","123456"],
-        name : ["신현태", "신개발", "왕현태"],
-    };
-    static getUsers(...fields){
-        const users =this.#users;
-        const newUsers= fields.reduce((newUsers,field) => {
-            if (users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        },{});
-        return newUsers;
-    }
-    static getUserInfo(id){
-        const users = this.#users;
+const fs = require("fs").promises;
+
+class UserStorage {
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
-        },{});
+        }, {});
+
         return userInfo;
     }
-    static save(userInfo){
-        const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.password.push(userInfo.password);
-        return {success : true};
+    static getUsers(...fields) {
+    // const users =this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+        if (users.hasOwnProperty(field)) {
+            newUsers[field] = users[field];
+        }
+        return newUsers;
+        }, {});
+        return newUsers;
     }
+    static getUserInfo(id) {
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
 }
 
+  static save(userInfo) {
+    // const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.password.push(userInfo.password);
+    return { success: true };
+  }
+}
 module.exports = UserStorage;
