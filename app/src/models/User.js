8d475a9dfe4e87e23fd715e7d1ef5f82f099c4a0
shyun <96578042+shyunte@users.noreply.github.com>
2022-06-07 00:57:11 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -20,10 +20,14 @@ class User{
         return {success: false, msg: "없는 아이디 입니다."};
     }
 
-    register(){
+    async register(){
         const client = this.body;
-        const response = UserStorage.save(client);
+        try{
+        const response = await UserStorage.save(client);
         return response;
+        }catch(err){
+            return { success : false, msg : err };
+        }
     }
 }
 module.exports = User;
